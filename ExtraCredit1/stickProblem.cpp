494bafe79af5073add97d54622b8830ca2009dce
Jakin Chan <JakinChan200@gmail.com>
2022-11-15 16:08:51 -0800

    Implemented 20 line solution

--- ExtraCredit1/stickProblem.cpp
+++ ExtraCredit1/stickProblem.cpp
@@ -5,6 +5,7 @@ Given a stick, split it in 2 random spots (the spots can be the same). What are 
 #include <iostream>
 #include <math.h>
 #include <chrono>
+#include <vector>
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -12,29 +13,55 @@ using namespace std;
 void ratioTriangle(){
     int count = 0;
     int iterations = 10000000;
-    float stick, stick1, stick2;
+    vector<float> stick(3);
     srand(time(0));
 
     for(int i = 0; i < iterations; i++){
-        stick1 = (float)rand()/RAND_MAX;
-        stick2 = (float)rand()/RAND_MAX;
-        if(stick1 > stick2){
-            stick1 -= stick2;
-        }else{
-            stick2 -= stick1;
-        }
-        stick = 1 - stick1 - stick2;
+        stick[0] = (float)rand()/RAND_MAX;
+        stick[1] = (float)rand()/RAND_MAX;
 
-        if(!(stick+stick1 <= stick2 || stick1+stick2 <= stick || stick+stick2 <= stick1)){
+        sort(stick.begin(), stick.end()-1);
+        stick[1] -= stick[0];
+        stick[2] = 1 - stick[0] - stick[1];
+
+        if(!(stick[2]+stick[0] <= stick[1] || stick[0]+stick[1] <= stick[2] || stick[2]+stick[1] <= stick[0]))
             count++;
-        }
     }
 
     cout << "With " << iterations << " iterations, the ratio is: " << fixed << setprecision(3) << (float)count / iterations << endl;
     return;
 }
+
+// void ratioTriangle(){
+//     int count = 0;
+//     int iterations = 100000000;
+//     float stick, stick1, stick2;
+//     srand(time(0));
+
+//     for(int i = 0; i < iterations; i++){
+//         stick1 = (float)rand()/RAND_MAX;
+//         stick2 = (float)rand()/RAND_MAX;
+        
+//         if(stick1 > stick2){
+//             stick1 -= stick2;
+//         }else{
+//             stick2 -= stick1;
+//         }
+//         stick = 1 - stick1 - stick2;
+
+//         if(!(stick+stick1 <= stick2 || stick1+stick2 <= stick || stick+stick2 <= stick1)){
+//             count++;
+//         }
+//     }
+
+//     cout << "With " << iterations << " iterations, the ratio is: " << fixed << setprecision(3) << (float)count / iterations << endl;
+//     return;
+// }
+
 int main(int argc, char** argv){
-    for(int i = 0; i < 10; i++){
+    int timesToRun = 10;
+
+    for(int i = 0; i < timesToRun; i++){
         ratioTriangle();
     }
     return 0;
